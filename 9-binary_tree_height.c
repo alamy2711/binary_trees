0dc045df8de48f91c8aae0a0029ba1a18ceea59b
Alamy <mestafaalami11@gmail.com>
2024-01-03 02:48:03 +0100

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,5 +14,5 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t leftHeight = binary_tree_height(tree->left);
 	size_t rightHeight = binary_tree_height(tree->right);
 
-	return (MAX(leftHeight, rightHeight) + 1);
+	return ((leftHeight > rightHeight ? leftHeight : rightHeight) + 1);
 }
