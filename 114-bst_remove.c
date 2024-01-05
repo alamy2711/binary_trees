a33dcf22571b648d8357d1b122d08f3697b9c440
Alamy <mestafaalami11@gmail.com>
2024-01-05 23:53:16 +0100

    Update 114-bst_remove.c

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,6 +1,18 @@
 #include "binary_trees.h"
 
 /**
+ * find_successor - Find the in-order successor of a node
+ * @node: Node to find the successor for
+ * Return: In-order successor
+ */
+bst_t *find_successor(bst_t *node)
+{
+	while (node->left)
+		node = node->left;
+	return (node);
+}
+
+/**
  * bst_remove - Removes a node from a Binary Search Tree
  * @root: pointer to the root node of the tree where you will remove a node
  * @value: value to remove in the tree
@@ -10,37 +22,37 @@
  */
 bst_t *bst_remove(bst_t *root, int value)
 {
-	bst_t *temp;
+	bst_t *successor, *temp;
 
 	if (root == NULL)
 		return (NULL);
 
-	if (value < root->n)
+	if (root->n > value)
 		root->left = bst_remove(root->left, value);
-	else if (value > root->n)
+	else if (root->n < value)
 		root->right = bst_remove(root->right, value);
 	else
 	{
-		if (root->left == NULL)
+		if (root->left != NULL && root->right != NULL)
 		{
-			temp = root->right;
-			free(root);
-			return (temp);
+			successor = find_successor(root->right);
+			root->n = successor->n;
+			root->right = bst_remove(root->right, successor->n);
 		}
-		else if (root->right == NULL)
+		else
 		{
-			temp = root->left;
-			free(root);
-			return (temp);
+			temp = root;
+
+			if (!root->left)
+				root = root->right;
+			else if (!root->right)
+				root = root->left;
+
+			if (root)
+				root->parent = temp->parent;
+
+			free(temp);
 		}
-
-		temp = root->right;
-		while (temp->left != NULL)
-			temp = temp->left;
-
-		root->n = temp->n;
-		root->right = bst_remove(root->right, temp->n);
 	}
-
 	return (root);
 }
